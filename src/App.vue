dfcec4ba77cbe0aaac25f7a2c078840d66742a9c
jiejie <189425098@qq.com>
2021-01-11 18:03:47 +0800

    为Nav组件添加样式，引入active-class

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 </script>
 
 <style lang="scss">
-//@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/reset.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <router-link to="/labels">
+  <nav>
+    <router-link to="/labels" class="item" active-class="selected">
       <Icons name="labels"/>
       Labels
     </router-link>
-    <router-link to="/money">
+    <router-link to="/money" class="item" active-class="selected">
       <Icons name="money"/>
       Money
     </router-link>
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icons name="statistics"/>
       Statistics
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -22,5 +22,24 @@ name: "Nav"
 </script>
 
 <style lang="scss" scoped>
-
+nav {
+  display: flex;
+  > .item {
+    padding: 2px 0;
+    width: 33.3333333%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 12px;
+    filter: grayscale(85%);
+  }
+  .icon {
+    width: 32px;
+    height: 32px;
+  }
+  .item.selected {
+    filter: grayscale(0);
+  }
+}
 </style>
