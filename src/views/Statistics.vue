c40e12dfa82dafba11aaef02621c560124a8160d
jiejie <189425098@qq.com>
2021-01-30 14:20:24 +0800

    完成数据统计页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,27 +1,60 @@
 <template>
-  <Layout class-prefix="statistics">
-    <Types/>
-    <TotalBar/>
-    <Details/>
-  </Layout>
+  <layout class-prefix="details">
+
+      <Types/>
+    <div class="wrapper">
+      <div class="dropdown">
+        <Dropdown :data-source="showYear"/>
+        <Dropdown :data-source="showMonth"/>
+      </div>
+      <DateLine/>
+      <DatePie/>
+    </div>
+  </layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import TotalBar from '@/components/TotalBar.vue';
+import DateLine from '@/components/echarts/DateLine.vue';
 import Types from '@/components/Types.vue';
-import Details from '@/components/statistics/Details.vue';
+import Dropdown from '@/components/details/Dropdown.vue';
+import DatePie from '@/components/echarts/DatePie.vue';
 @Component({
-  components: {Details, Types, TotalBar}
+  components: {DatePie, Types, DateLine,Dropdown}
 })
 export default class Statistics extends Vue{
-}
+  get showMonth() {
+    return this.$store.state.showMonth;
+  }
+  get showYear() {
+    return this.$store.state.showYear;
+  }
+  created() {
+    this.$store.commit('fetchYear');
+    this.$store.commit('fetchMonth');
+  }
+};
 </script>
 
 <style lang="scss">
-.statistics-layout {
+.details-layout {
   display: flex;
   flex-direction: column;
 }
+</style>
+
+<style lang="scss" scoped>
+  .wrapper {
+    flex-grow: 1;
+    overflow: auto;
+    .dropdown {
+      margin: auto;
+      display: flex;
+      width: 30%;
+      justify-content: space-between;
+      text-align: center;
+      padding: 10px 0;
+    }
+  }
 </style>
